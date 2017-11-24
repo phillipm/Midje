7388e48c9a78d8fea37e13867d8e7f7193612312
Phillip Mates <phillip.mates@nubank.com.br>
2017-11-24 11:50:00 +0100

    example of failing tests that should work

--- test/as_documentation/about_defrecord/using_refer__provided_tests/test.clj
+++ test/as_documentation/about_defrecord/using_refer__provided_tests/test.clj
@@ -20,3 +20,31 @@
     (provided
       (bump rec) => -1)))
 
+
+(defprotocol Caller
+  (call [caller x]))
+
+(defrecord ACaller [func]
+  Caller
+  (call [_ x] (func x)))
+
+(defn fack-func [args] 'shouldnt-return)
+
+(facts "Mock a func call inside a inline record"
+  (against-background [(fack-func 1) => 100]
+    (fact "against-background"
+      (call (->ACaller fack-func) 1) => 100))
+
+  (fact "provided"
+    (call (->ACaller fack-func) 1) => 100
+    (provided (fack-func 1) => 100)))
+
+(def caller-instance (->ACaller fack-func))
+(facts "Mock a func call inside a record reference"
+  (against-background [(fack-func 1) => 100]
+    (fact "against-background"
+      (call caller-instance 1) => 100))
+
+  (fact "provided"
+    (call caller-instance 1) => 100
+    (provided (fack-func 1) => 100)))
