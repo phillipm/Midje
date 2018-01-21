eb16a155e08bdd4ac7529b6fcdfb9444ccf5b9dc
Phillip Mates <phillip.mates@nubank.com.br>
2018-01-21 16:36:53 +0100

    replace tree-seq w/ func that doesn't iterate over more data than needed

--- src/midje/parsing/1_to_explicit_form/metaconstants.clj
+++ src/midje/parsing/1_to_explicit_form/metaconstants.clj
@@ -2,8 +2,17 @@
   midje.parsing.1-to-explicit-form.metaconstants
   (:require [midje.data.metaconstant :as data]))
 
+(defn- nested-tree-filter
+  [branch? pred? root]
+  (let [walk (fn walk [node]
+               (if (branch? node)
+                 (apply clojure.set/union (map walk node))
+                 (when (pred? node)
+                   #{node})))]
+    (walk root)))
+
 (defn predefine-metaconstants-from-form [form]
-  (let [metaconstant-symbols (set (filter data/metaconstant-symbol? (tree-seq coll? seq form)))]
+  (let [metaconstant-symbols (nested-tree-filter coll? data/metaconstant-symbol? form)]
     (doseq [symbol metaconstant-symbols]
       (intern *ns* symbol (data/metaconstant symbol {} nil)))
     metaconstant-symbols))
